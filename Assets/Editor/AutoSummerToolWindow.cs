1e243ea36c6be985831a0c3bf2d4d5da0c27bd1f
qq21 <237041086@qq.com>
2019-02-17 11:41:59 +0800

    AutoSummaryTools

---
+++ Assets/Editor/AutoSummerToolWindow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+public class AutoSummerToolWindow : EditorWindow {
+
+    // Use this for initialization
+
+    string youName;
+     [MenuItem("Author/AuthorInfo")]
+    public static void ShowAuthor()
+    {
+        //创建一个 MyWindow的可停靠窗口
+        //EditorWindow.GetWindow(typeof(AutoSummerToolWindow));
+        //Unility 是否为 工具窗口 (不可停靠)
+        //EditorWindow.GetWindow(typeof(AutoSummerToolWindow), false, "AuthorInfo Window");
+        GetWindowWithRect(typeof(AutoSummerToolWindow), new Rect(0, 0, 200, 200));
+    }
+
+    private void OnGUI()
+    {
+        Debug.Log("OnGUI");
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);//粗体     
+         youName = EditorGUILayout.TextField("作者:", youName);
+        if (GUILayout.Button(new GUIContent("确定")))
+        {
+            //string tempPath = File.ReadAllText(Application.dataPath + "/Editor/Recent.txt");
+
+            //string comtent = File.ReadAllText(tempPath);
+ 
+            //Debug.Log(comtent);
+            //if (!comtent.Contains(youName))
+            //{
+            //    if (!comtent.Contains(AutoSummerTools.annotationStr))
+            //    {
+            //        AutoSummerTools.youName = youName;
+            //        AutoSummerTools.ChangeFileSummer("Done", tempPath,youName);
+            //        Debug.Log(AutoSummerTools.youName);
+            //    }
+            //    else
+            //    {
+            //        Debug.Log("1");
+            //        comtent = comtent.Replace(AutoSummerTools.youName, youName);
+            //        File.WriteAllText(tempPath, comtent);
+            //        Debug.Log(comtent);
+
+            //    }
+
+
+            //}
+
+
+          
+        
+ 
+        }
+
+
+
+        EndWindows();
+
+    }
+
+    public void Callback(object obj)
+    {
+        Debug.Log("Selected: " + obj);
+    }
+}
---
+++ Assets/Editor/AutoSummerTools.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class AutoSummerTools : UnityEditor.AssetModificationProcessor {
+
+    
+
+    public static string annotationStr =
+     "///<summary>" +
+     "描述:\r\n" +
+     "///" + "作者:"+"\r\n" +
+     "///" + "创建时间: #CreateTime# \r\n" +
+     "///" + "版本:v1.0\r\n" +
+     "///</summary>";
+    
+    public static void ChangeFileSummer(object o,string path)
+    {
+
+        Debug.Log(path);
+        if (File.Exists(path))
+        {
+            if (path.EndsWith(".cs"))
+            {
+                annotationStr = annotationStr.Replace("#CreateTime#", System.DateTime.Now.ToString("yyy/MM/dd HH:ss"));
+
+               
+                annotationStr += File.ReadAllText(path);
+                
+                File.WriteAllText(path, annotationStr);
+                Debug.Log(o);
+            }
+        }
+    }
+ 
+
+
+    public static void OnWillCreateAsset(string name)
+    {
+     
+
+        string path = name.Replace(".meta", "");
+        Debug.Log(path);
+        GenericMenu genericMenu = new GenericMenu();
+
+        Debug.Log(Application.dataPath);
+        if (File.Exists(Application.dataPath + "/Editor/Recent"))
+        {
+            File.WriteAllText(Application.dataPath + "/Editor/Recent.txt", path);
+        }
+        else
+        {
+            File.Create(Application.dataPath + "/Editor/Recent");
+            File.WriteAllText(Application.dataPath + "/Editor/Recent.txt", path);
+        }
+
+        genericMenu.AddItem(new GUIContent("确定"), false,
+        (object o) 
+        =>
+            {
+                ChangeFileSummer(o,path);
+            } ,"Done");
+
+        genericMenu.AddSeparator("");
+        genericMenu.AddItem(new GUIContent("取消"), false, () => { return; });
+        genericMenu.AddItem(new GUIContent("修改", "呼出作者菜单"), false, () => {
+          
+        Debug.Log(path);
+        EditorWindow.GetWindow(typeof(AutoSummerToolWindow));
+           
+        });
+        genericMenu.ShowAsContext();
+    }
+
+
+
+    public static void OnGUI()
+    {
+        EditorGUILayout.TextField("name");
+    }
+    public void Callback(object obj)
+    {
+        Debug.Log("Selected: " + obj);
+    }
+}
